a33a748e7c531935f8003d4dd84acb609394b90e
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-04-08 23:26:40 -0400

    dev api

--- config.js
+++ config.js
@@ -9,4 +9,12 @@ angular.module('app').config(function($locationProvider, $routeProvider) {
 			templateUrl: 'views/index.html',
 			controller: 'IndexCtrl'
 		});
+}).run(function(TodosStore) {
+	window.getState = function() {
+		return JSON.stringify(TodosStore.getState());
+	};
+
+	window.setState = function(state) {
+		return TodosStore.injectState(JSON.parse(state));
+	};
 });
